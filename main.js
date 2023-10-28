a5d656efae1ac4c145dc9cbf23ed95da158bf064
Chelsea <chelsea.fitzy1011@gmail.com>
2023-10-29 01:38:56 +0800

    javascript_imgSlider

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 console.log("JavaScript is working!")
+
+// Glider configuration
+new Glider(document.querySelector('.glider'), {
+
+    slidesToShow: 1,
+    draggable: true,
+    dots: '#dots',
+    arrows: {
+      prev: ".glider-prev",
+      next: ".glider-next"
+    },
+  
+    // Responsive breakpoints
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1.5,
+          scrollLock: false,
+        }
+      }
+    ]
+  });
